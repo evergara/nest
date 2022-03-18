6a15ce1ccc74228bd5645831c44397f228b8c85e
Emerson Vergara <ee-web@hotmail.com>
2022-03-17 20:06:25 -0500

    Configuracion base de proyecto

---
+++ api_todo/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: true,
+    aplication:{
+        name: 'Api',
+        context_path: 'Api',
+        port: '3003'
+    },
+    database:{
+      type: "DATABASE_TYPE",
+      host: "DATABASE_HOST",
+      port: "DATABASE_PORT",
+      name: "DATABASE_NAME",
+      user: "DATABASE_USER",
+      password: "DATABASE_PASSWORD"
+    },
+  };
---
+++ api_todo/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+    aplication:{
+        name: 'Api',
+        context_path: 'Api',
+        port: '3003'
+    },
+    database:{
+      type: "DATABASE_TYPE",
+      host: "DATABASE_HOST",
+      port: "DATABASE_PORT",
+      name: "DATABASE_NAME",
+      user: "DATABASE_USER",
+      password: "DATABASE_PASSWORD"
+    },
+  };
