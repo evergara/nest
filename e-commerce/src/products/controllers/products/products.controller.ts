5a5f14e0ca0b6b210f0717844106e3b302618b76
Emerson Vergara <ee-web@hotmail.com>
2022-03-31 17:59:43 -0500

    maneno de decorador @Post, @Put, @Delete, @Get

--- e-commerce/src/products/controllers/products/products.controller.ts
+++ e-commerce/src/products/controllers/products/products.controller.ts
@@ -1,4 +1,81 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { response } from 'express';
+
+import { CreateProductDto } from './../../dtos/product.dto';
+import { UpdateProductDto } from '../../dtos/product.dto';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor() {}
+
+  @Get()
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return {
+      message: 'Listado de productos',
+      filters: {
+        limit,
+        offset,
+        brand,
+      },
+    };
+  }
+
+  @Get('filter')
+  getProductFilter() {
+    return {
+      message: `yo soy un filter`,
+    };
+  }
+
+  @Get('/:productId')
+  @HttpCode(HttpStatus.ACCEPTED)
+  getOne(@Param('productId', ParseIntPipe) productId: number) {
+    // return response.status(200).send({
+    //   message: `product ${productId}`,
+    //});
+    return {
+      message: `product ${productId}`,
+    };
+  }
+
+  @Post()
+  create(@Body() payload: CreateProductDto) {
+    return {
+      message: 'accion de crear',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') productId: string, @Body() payload: UpdateProductDto) {
+    return {
+      message: {
+        id: productId,
+        payload,
+      },
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') productId: string) {
+    return {
+      message: `product ${productId}`,
+    };
+  }
+}
--- e-commerce/src/products/dtos/product.dto.ts
+++ e-commerce/src/products/dtos/product.dto.ts
@@ -1,4 +1,12 @@
-export class ProductDto {
+export class CreateProductDto {
+  readonly name: string;
+  readonly description: string;
+  readonly price: number;
+  readonly stock: number;
+  readonly image: string;
+}
+
+export class UpdateProductDto {
   readonly name: string;
   readonly description: string;
   readonly price: number;
