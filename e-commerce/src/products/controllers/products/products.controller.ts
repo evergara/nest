8d10d864d1acba451f019d83b936f4052a3112b7
Emerson Vergara <ee-web@hotmail.com>
2022-03-31 19:23:23 -0500

    Implementando servicios en tu controlador

--- e-commerce/src/products/controllers/products/products.controller.ts
+++ e-commerce/src/products/controllers/products/products.controller.ts
@@ -16,10 +16,12 @@ import { Response } from 'express';
 
 import { CreateProductDto } from './../../dtos/product.dto';
 import { UpdateProductDto } from '../../dtos/product.dto';
+import { ProductsService } from 'src/products/services/products/products.service';
+import { Product } from '../../entities/product.entity';
 
 @Controller('products')
 export class ProductsController {
-  constructor() {}
+  constructor(private productsService: ProductsService) {}
 
   @Get()
   getProducts(
@@ -27,6 +29,7 @@ export class ProductsController {
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
+
     return {
       message: 'Listado de productos',
       filters: {
@@ -34,6 +37,9 @@ export class ProductsController {
         offset,
         brand,
       },
+      product: [
+        this.productsService.findAll()
+      ]
     };
   }
 
@@ -50,34 +56,37 @@ export class ProductsController {
     @Param('productId', ParseIntPipe) productId: number,
     @Res() response: Response,
   ) {
+
+    let product: Product = this.productsService.findOne(productId);
     response.status(HttpStatus.OK).send({
-      message: `product ${productId}`,
+      product
     });
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() payload: CreateProductDto) {
+    let product: Product = this.productsService.create(payload);
     return {
       message: 'accion de crear',
-      payload,
+      product
     };
   }
 
   @Put(':id')
-  update(@Param('id') productId: string, @Body() payload: UpdateProductDto) {
+  update(@Param('id', ParseIntPipe)  productId: number, @Body() payload: UpdateProductDto) {
+    let product: Product = this.productsService.update(productId, payload);
     return {
       message: {
         id: productId,
-        payload,
+        product,
       },
     };
   }
 
   @Delete(':id')
-  delete(@Param('id') productId: string) {
-    return {
-      message: `product ${productId}`,
-    };
+  delete(@Param('id', ParseIntPipe)  productId: number) {
+    let isRemove = this.productsService.remove(productId);
+    return isRemove;
   }
 }
--- e-commerce/src/products/services/products/products.service.ts
+++ e-commerce/src/products/services/products/products.service.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import {
+  CreateProductDto,
+  UpdateProductDto,
+} from 'src/products/dtos/product.dto';
+import { Product } from 'src/products/entities/product.entity';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  private counterId = 1;
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'bla bla',
+      price: 122,
+      image: '',
+      stock: 12,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      description: 'bla bla',
+      price: 1000,
+      image: '',
+      stock: 10,
+    }
+  ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    const product = this.products.find((item) => item.id === id);
+    if (!product) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+    return product;
+  }
+
+  create(payload: CreateProductDto) {
+    console.log(payload);
+    this.counterId = this.products.length + 1;
+    const newProduct = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  update(id: number, payload: UpdateProductDto) {
+    const product = this.findOne(id);
+    const index = this.products.findIndex((item) => item.id === id);
+    this.products[index] = {
+      ...product,
+      ...payload,
+    };
+    return this.products[index];
+  }
+
+  remove(id: number) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+    this.products.splice(index, 1);
+    return true;
+  }
+}
